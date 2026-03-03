6fb420361eead0f9f0adc15a1b495ef495d04efe
eragon11145 <109441971+eragon11145@users.noreply.github.com>
2026-03-02 19:51:28 -0800

    Update and rename randommon.js to randommon2.js

---
+++ randommon2.js
@@ -0,0 +1,14 @@
+<script>
+  // 1. Create an array with the paths to your images
+  var images = [
+    "zeraora.gif",
+    "fluttermane.gif",
+    "tapufini.gif",
+    "incineroar.gif",
+    "landorus.gif"
+  ];
+
+  var randomIndex = Math.floor(Math.random() * images.length);
+
+  return(images[randomIndex])
+</script>
