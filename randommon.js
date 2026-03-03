7bc7ba19245f19e3581a198e88b679a02e41d065
eragon11145 <109441971+eragon11145@users.noreply.github.com>
2026-03-02 19:35:05 -0800

    Create randommon.js

---
+++ randommon.js
@@ -0,0 +1,14 @@
+<script>
+  // 1. Create an array with the paths to your images
+  var images = [
+    "https://play.pokemonshowdown.com/sprites/ani/zeraora.gif",
+    "https://play.pokemonshowdown.com/sprites/ani/fluttermane.gif",
+    "https://play.pokemonshowdown.com/sprites/ani/tapufini.gif",
+    "https://play.pokemonshowdown.com/sprites/ani/incineroar.gif",
+    "https://play.pokemonshowdown.com/sprites/ani/landorus.gif"
+  ];
+
+  var randomIndex = Math.floor(Math.random() * images.length);
+
+  document.getElementById("randomImage").src = images[randomIndex];
+</script>
